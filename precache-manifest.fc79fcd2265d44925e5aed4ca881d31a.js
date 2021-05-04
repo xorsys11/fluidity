309b72449b7076b38a66f319dae941f6f88946bf
xorsys <xorsys@pop-os.localdomain>
2021-05-04 20:48:02 +0530

    Updates

---
+++ precache-manifest.fc79fcd2265d44925e5aed4ca881d31a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34774769ecc36872e26dc660e00c2780",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9cdc0067ec4ba605d69",
+    "url": "./static/css/main.6cbbedc2.chunk.css"
+  },
+  {
+    "revision": "3837c85bfc94dcd7d15a",
+    "url": "./static/js/2.a13a3398.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.a13a3398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9cdc0067ec4ba605d69",
+    "url": "./static/js/main.ded89be7.chunk.js"
+  },
+  {
+    "revision": "8176c60e066153806f52",
+    "url": "./static/js/runtime-main.bc88bcec.js"
+  },
+  {
+    "revision": "6214fef263bf351d32d599371642be62",
+    "url": "./static/media/FiraCode-Light.6214fef2.woff"
+  },
+  {
+    "revision": "3da66ca1931ea28aafb1c6e9cdf1d0f8",
+    "url": "./static/media/FiraCode-Regular.3da66ca1.woff"
+  },
+  {
+    "revision": "1a837031f89d2b862e46efbfeee89644",
+    "url": "./static/media/duckduckgo.1a837031.svg"
+  },
+  {
+    "revision": "0c182f86f349bafa00ba5201f69c79b6",
+    "url": "./static/media/google.0c182f86.svg"
+  },
+  {
+    "revision": "06ef25683d4f965ec5c9f7041f277950",
+    "url": "./static/media/logo.06ef2568.png"
+  },
+  {
+    "revision": "03400a86da3daf17b376b34055f6732f",
+    "url": "./static/media/pic_1.03400a86.jpg"
+  },
+  {
+    "revision": "42b93d95319153fe54b47953aad4ed1b",
+    "url": "./static/media/pic_2.42b93d95.jpg"
+  },
+  {
+    "revision": "8796fde5bffabc5215bc0aad44d1a142",
+    "url": "./static/media/pic_3.8796fde5.jpg"
+  },
+  {
+    "revision": "66ccb182a56965ca730c5fecb7f7862a",
+    "url": "./static/media/pic_4.66ccb182.jpg"
+  },
+  {
+    "revision": "6e9543cd93106257d6783bd347446d27",
+    "url": "./static/media/pic_5.6e9543cd.jpg"
+  },
+  {
+    "revision": "65b20253b182ec977b99709892c72524",
+    "url": "./static/media/pic_6.65b20253.jpg"
+  },
+  {
+    "revision": "6516acf69228e1450d3b7c6f1ce7f475",
+    "url": "./static/media/pic_7.6516acf6.jpg"
+  },
+  {
+    "revision": "ba1342f42d643e4f554eaf120c6a2a38",
+    "url": "./static/media/pic_8.ba1342f4.png"
+  },
+  {
+    "revision": "7b2c220e78a60f719f0e5151da61ccc9",
+    "url": "./static/media/qwant.7b2c220e.svg"
+  }
+]);
